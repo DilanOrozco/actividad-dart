71bef99bbe82c07994c47346b2bc73dc4db6e8b5
palaciomazosamuel <palaciomazosamuel@gmail.com>
2026-02-20 11:41:34 -0500

    fix feat: create point 2

--- punto2.dart
+++ punto2.dart
@@ -1,7 +1,7 @@
 // #2. Se necesita obtener el promedio de un estudiante a partir de sus tres notas parciales. El
 // estudiante debe digitar sus tres notas y el sistema deberá darle el promedio del semestre.
-
-void main() {
+import 'dart:io'; 
+void Punto2() {
   print("Digite la primera nota:");
   double nota1 = double.parse(stdin.readLineSync()!);
 
