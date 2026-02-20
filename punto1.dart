f2d08be4a5af984cee72f9664c96e8523b85e756
AlejandroSuarez7 <alejandro.suarez0321@gmail.com>
2026-02-20 10:53:26 -0500

    iniciando

---
+++ punto1.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void calcularDistancia(){
+    print("ingresa la velocidad del automóvil en m/s ");
+    double V = double.parse(stdin.readLineSync()!);
+    
+    print("ingresa el tiempo que el automóvil ha estado en movimiento en segundos ");
+    double T = double.parse(stdin.readLineSync()!);
+    
+    double D = V * T;
+    print("La distancia recorrida por el automóvil es $D metros");
+}
+
+
+
+
